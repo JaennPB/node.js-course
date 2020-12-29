3455711aa19dae35358701f3bb2d5a490fb69a73
jaennpb <jaenn.bucio@gmail.com>
2020-12-29 12:01:38 -0600

    commit 3

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // getting the fs module from node
 // will return an object which will give us access to methods from module
+
 // require() function is the easiest way to include modules that exist in different files
 const fs = require("fs");
+const http = require("http");
+
+// *******************************************
+// ************************************* files
 
 // ********************* blocking synchronous way
 
@@ -20,13 +25,27 @@ fs.writeFileSync("./txt/output.txt", textOut);
 fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
   if (err) return console.log("Error 💥");
   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
+    // console.log(data2);
     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log(data3);
+      //   console.log(data3);
       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("File was written");
+        // console.log("File was written");
       });
     });
   });
 });
 console.log("reading file...");
+
+// *******************************************
+// ************************************ server
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end("Hello, master. I am the new server you just created");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
+
+// by using 127.0.0.1:8000 as a url, a new request is created, and we get the response from created server
