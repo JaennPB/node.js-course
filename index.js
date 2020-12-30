00ba4d1f119fd9241ab311f5366c3d85357d3144
jaennpb <jaenn.bucio@gmail.com>
2020-12-29 18:38:08 -0600

    commit 1

--- index.js
+++ index.js
@@ -3,17 +3,46 @@ const url = require("url");
 const fs = require("fs");
 const { type } = require("os");
 
+// *************************************************** SYNCHRONOUSLY READING DATA
+// ********************* JSON DATA
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
 
+// ********************* OVERVIEW
+const temOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+// ********************* CARD
+const temCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+// ********************* PRODUCT
+const temProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+// *************************************************** CREATING SERVER AND ROUTING
+// ********************* CREATE SERVER
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // OVERVIEW
   if (pathName === "/" || pathName === "/overview") {
     res.end("This is the overview");
+
+    // PRODUCTS
+  } else if (pathName === "/products") {
+    res.end("This is the overview");
+
+    // API
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
+
+    // NOT FOUND
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
